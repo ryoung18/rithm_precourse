249c7db3bb1ea99b0339267037fa440a2696433e
Renwick Young <young.renwick@gmail.com>
2017-07-23 13:20:43 -0700

    complete tic tac toe

--- intermediate-javascript-part1/final_project/script.js
+++ intermediate-javascript-part1/final_project/script.js
@@ -1,28 +1,32 @@
-// window.onload = function (){
+window.onload = function (){
 
   let turn = 0;
-  let pSel= [];
-  let player = function (){ return turn % 2 === 0 ? "X" : "O" }
- 
+  let pSel= []; //player Selection
+  let player = function (){return turn % 2 === 0 ? "X" : "O"};
+  let showPlayer = document.getElementById("showPlayer")
 
   //player selection - addEventListener
     //record player selection in playerSelection array
     //mark player selection on webpage
     //show next player on webpage
+  for(let i = 0; i < 9; i++){
+    let selections = document.getElementById(i);
 
-    function playerChoice(num){
-      pSel[num] = player();
-      checkWinner();
-    }
-
-
-
+    selections.addEventListener("click", function (){
+      if(pSel[i] === undefined){
+        selections.innerText = player();
+        pSel[i] = player();
+        checkWinner() //hmmm alert displays before updating innerText.
+      }
+    })
+  }
   //check if won
     //min 5 turns to win.
       // winning combinations 012, 036, 048, 147, 246, 258, 345, 678
   function checkWinner(){
     let check = player().repeat(3)
-
+    let currentPlayer = player();
+  
     if(turn >= 5-1){
       if(
           pSel[0]+pSel[1]+pSel[2] === check ||
@@ -34,31 +38,20 @@
           pSel[2]+pSel[5]+pSel[8] === check ||
           pSel[6]+pSel[7]+pSel[8] === check 
           ) {
-         console.log("Player " + player() + " Wins!" )
+         turn = 9
+         return alert("Player " + currentPlayer + " Wins!" )
+      } else if (turn === 8){
+        return alert("Tie")
       }
-    } else {
-      turn++
     }
+      turn++;
+      showPlayer.innerHTML = player();
   }
       
 
    
 
-// }
-playerChoice(0);
-
-
-playerChoice(8);
-
-
-playerChoice(1);
-
-
-playerChoice(7);
-
-
-
-playerChoice(2);
+}
 
 
 
